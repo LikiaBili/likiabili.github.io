e69e3d9dc673b4050c9f4112ddc5ddd0789e1ede
Likia <103659676+LikiaBili@users.noreply.github.com>
2023-11-04 03:16:38 +0800

    Page update1 (#1)
    * Write something lol
    
    * Update main.js, again...
    
    * Update main.css
    
    * Removing repeative png files, add New Domain section in site:root
    
    * Whole site update
    
    * edit devlog publish time

--- hub.js
+++ hub.js
@@ -1,6 +1,4 @@
-function hoverIn(id){
-    document.getElementById(id).innerText = "HOVERED!";
-}
-function hoverOut(id){
-
-}
+const customMotd = ["We rise","Lorem ipsum","We are not just games","What the hell are you doing",
+    "SMG4!!!!!!!!!!!!!!","THE END IS NEVER THE END IS NEVER THE END IS NEVER THE END IS NEVER THE END",
+    "<a href='https://space.bilibili.com/14444480'>[[Hyperlink Blocked]]</a>"];
+document.getElementById("motd").innerHTML = customMotd[Math.round(Math.random()*1000) % customMotd.length];
