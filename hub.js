40fba6f086f12917638d9bbd5bd1034fe6dfd6fe
Likia <likia_uploader@outlook.com>
2023-11-04 03:19:29 +0800

    reupload files to github

--- hub.js
+++ hub.js
@@ -1,4 +1,5 @@
 const customMotd = ["We rise","Lorem ipsum","We are not just games","What the hell are you doing",
     "SMG4!!!!!!!!!!!!!!","THE END IS NEVER THE END IS NEVER THE END IS NEVER THE END IS NEVER THE END",
     "<a href='https://space.bilibili.com/14444480'>[[Hyperlink Blocked]]</a>"];
+
 document.getElementById("motd").innerHTML = customMotd[Math.round(Math.random()*1000) % customMotd.length];
