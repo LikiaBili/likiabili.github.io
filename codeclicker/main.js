d29b1db6d043d36b58dff8c9833bf0ddcc7ab086
Likia <likia_uploader@outlook.com>
2023-11-04 03:03:13 +0800

    Whole site update

--- codeclicker/main.js
+++ codeclicker/main.js
@@ -4,23 +4,30 @@ let bytesPerClick = 1;
 let bps = 0;
 let rawbps = 0; //without multiplyer, for xp caculation
 let bigConsole = document.getElementById("console");
-var buildings = {keyboard:{basePrice:15,bps:0.1,rawbps:0.1,amount:0,price:0},
-                programmer:{basePrice:150,bps:1,rawbps:1,amount:0,price:0}};
+var buildings = {
+    keyboard:{basePrice:15,bps:0.1,rawbps:0.1,amount:0,price:0},
+    programmer:{basePrice:150,bps:1,rawbps:1,amount:0,price:0}};
 var hoverMessages = {
-    build_1_shop:{get:function(){
+    build_1_shop:{get(){
         calcBuildingPrice('keyboard');
         return {icon:"img/buildings/keyboard.png",
                title:"Keyboard",
-               desc:"A keyboard writes one line of code per ten seconds.<br>Making ${buildings['keyboard']['bps'] * buildings['keyboard']['amount']}<br>Produce: ${buildings['keyboard']['bps']} bytes per second"};
+               desc:"A keyboard auto writes one line of code per ten seconds.<br><br>" +
+                   "You owned "+buildings['keyboard']['amount']+" which is making "+buildings['keyboard']['bps'] * buildings['keyboard']['amount']+" bytes per second <br>" +
+                   "Type out "+buildings['keyboard']['bps']+" bytes every second each"};
     }},
-    build_2_shop:{get:function(){
+    build_2_shop:{get(){
         calcBuildingPrice('programmer');
         return {icon:"img/console.png",
                title:"Programmer",
-               desc:"A nice programmer to write more code.<br>Making ${buildings['programmer']['bps'] * buildings['programmer']['amount']}<br>Produce: ${buildings['programmer']['bps']} bytes per second"};
+            desc:"A nice programmer to code.<br><br>" +
+                "You owned "+buildings['programmer']['amount']+" which is making "+buildings['programmer']['bps'] * buildings['programmer']['amount']+" bytes per second <br>" +
+                "Type out "+buildings['programmer']['bps']+" bytes every second each"};
     }}};
 let hovering = 0;
 let hoverDisplayElement = document.getElementById('hoverTooltip');
+var Mouse = {X:0,Y:0};
+var Screen = {X:0,Y:0};
 
 function handleClick() {
     codes = codes + 1;
@@ -38,38 +45,59 @@ function hoverOut(id){
   }
 }
 
+function logMouseMove(e){
+    Mouse.X = e.clientX;
+    Mouse.Y = e.clientY;
+    Screen.X = e.screenX;
+    Screen.Y = e.screenY;
+}
+
 function buildingUpdate(){ 
     //this does the price, bps recaculate etc.
     var tmpBps = 0;
+    var rawBpsTmp = 0;
+    var curBps;
     for(id in buildings){
-        tmpBps += buildings[id]['bps'] * buildings[id]['amount'];
+        calcBuildingPrice(id);
+        curBps = buildings[id]['bps'] * buildings[id]['amount'];
+        rawBpsTmp += buildings[id]['rawBps'] * buildings[id]['amount'];
+        document.getElementById(id+"_cost").innerText = buildings[id]['price'] + "Bytes";
+
+        tmpBps += curBps;
     }
     bps = Math.round(tmpBps*10)/10;
+    rawBps = Math.round(rawBpsTmp*10)/10;
 }
 function tenPerSecondUpdate(){
     //caculation
-    bps = buildings['keyboard']['amount']*buildings['keyboard']['bps'];
-    rawbps = buildings['keyboard']['amount']*buildings['keyboard']['rawbps'];
     bytes += bps/10;
     codes += rawbps/10;
-    bytes = Math.round(bytes*10)/10;
-    codes = Math.round(codes*10)/10;
+    bytes = Math.round(bytes*100)/100;
+    codes = Math.round(codes*100)/100;
     
     //display
-    bytesDisplay.innerText = bytes + " bytes";
-    linesDisplay.innerText = codes + " lines";
-    if(bps != 0) {
-        bpsDisplay.innerText = "bps: " + bps;
+    document.getElementById("bytesDisplay").innerText = Math.round(bytes) + " bytes";
+    document.getElementById("linesDisplay").innerText = Math.round(codes) + " lines";
+    if(bps !== 0) {
+        document.getElementById("bpsDisplay").innerText = "bps: " + bps;
     }
 }
 function hoverUpdate(){
     if(hovering != 0){
-      
+        hoverDisplayElement.style.top = (Mouse.Y+20)+"px";
+        hoverDisplayElement.style.left = (Mouse.X+20)+"px";
+        hoverDisplayElement.style.display = "initial";
+        let tooltip = hoverMessages[hovering].get();
+        document.getElementById("hoverTitle").innerHTML = tooltip['title'];
+        document.getElementById("hoverDesc").innerHTML = tooltip['desc'];
+        document.getElementById("hoverIcon").src = tooltip['icon'];
+    }else{
+        hoverDisplayElement.style.display = "none";
     }
 }
 
 function calcBuildingPrice(id){
-    buildings[id]['price'] = buildings[id]['basePrice']*(1.15 ** buildings[id]['amount']);
+    buildings[id]['price'] = Math.round(buildings[id]['basePrice']*(1.15 ** buildings[id]['amount']));
 }
 function buyBuilding(id){
     calcBuildingPrice(id);
@@ -79,8 +107,12 @@ function buyBuilding(id){
     }
 }
 
-setInterval(hoverUpdate,50);
-setInterval(tenPerSecondUpdate,100);
-setInterval(buildingUpdate,1000);
+var intervalFunctions = {hover:{func:hoverUpdate,time:50},hundms:{func:tenPerSecondUpdate,time:100},building:{func:buildingUpdate,time:500}};
+for(i in intervalFunctions){
+    intervalFunctions[i].func();
+    setInterval(intervalFunctions[i].func,intervalFunctions[i].time);
+}
+
+document.addEventListener("mousemove", logMouseMove);
 
 document.getElementById('LoadingScreen').remove();
