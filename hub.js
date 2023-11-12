4391e4d57943d6c31bdcce6af32c54248a67c0ea
BiliOrange <90908765+orangeZSCB@users.noreply.github.com>
2023-11-12 18:25:33 +0800

    Add 2 Motd (#7)

--- hub.js
+++ hub.js
@@ -1,4 +1,4 @@
 const customMotd = ["We rise","Lorem ipsum","We are not just games","What the hell are you doing",
-    "SMG4!!!!!!!!!!!!!!","THE END IS NEVER THE END IS NEVER THE END IS NEVER THE END IS NEVER THE END",
+    "SMG4!!!!!!!!!!!!!!","THE END IS NEVER THE END IS NEVER THE END IS NEVER THE END IS NEVER THE END","1145141919810","WELCOME TO <del>OSU</del>Likia's Site!!!","Beep beep I'm a sheep i said Beep beep I'm a sheep",
     "<a href='https://space.bilibili.com/14444480'>[[Hyperlink Blocked]]</a>"];
 document.getElementById("motd").innerHTML = customMotd[Math.round(Math.random()*1000) % customMotd.length];
